ceb7677ae28133f821885e140cb03648222d0e22
Dilan Sandaruwan <mnd.sanda94@gmail.com>
2024-04-26 16:38:50 +0530

    update Gradle, AGP, and Kotlin plugin versions

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/viewmodel/home/HomeViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/viewmodel/home/HomeViewModel.kt
@@ -34,7 +34,7 @@ class HomeViewModel(
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
+            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
                 header("Accept", "application/vnd.github.v3+json")
                 parameter("q", inputText)
             }
